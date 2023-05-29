589454c7c9d9cf3155d30bf4856b4252237c5ece
eswaribala <parameswaribala@gmail.com>
2023-05-29 13:10:34 +0530

    updated

--- InventoryService/InventoryService/Models/Product.cs
+++ InventoryService/InventoryService/Models/Product.cs
@@ -1,6 +1,20 @@
-﻿namespace InventoryService.Models
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace InventoryService.Models
 {
+    [Table("Product")]
     public class Product
     {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        [Column("Product_Id")]
+        public long ProductId { get; set; }
+        [Column("Name",TypeName ="varchar(200")]
+        public string? Name { get; set; }
+        //value object
+        public ProductDescription? ProductDescription { get; set; }
+        public string? SKU { get; set; }
+
     }
 }
