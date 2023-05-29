1fde394f31397120578587c40bb05ef7f1b56bbc
eswaribala <parameswaribala@gmail.com>
2023-05-29 13:16:16 +0530

    updated

--- InventoryService/InventoryService/Models/Product.cs
+++ InventoryService/InventoryService/Models/Product.cs
@@ -1,5 +1,6 @@
 ﻿using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
+using System.Data.Common;
 
 namespace InventoryService.Models
 {
@@ -15,6 +16,11 @@ namespace InventoryService.Models
         //value object
         public ProductDescription? ProductDescription { get; set; }
         public string? SKU { get; set; }
+
+        [ForeignKey("Category")]
+        [Column("Category_Id_FK")]
+        public long CategoryId { get; set; }
+        public Category? Category { get; set; }
 
     }
 }
