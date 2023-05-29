dba8317cfb53c612ebc3d23fc72c0fb95fea5937
eswaribala <parameswaribala@gmail.com>
2023-05-29 08:24:46 +0530

    updated

--- EcommerceConsoleApp/EcommerceConsoleApp/Program.cs
+++ EcommerceConsoleApp/EcommerceConsoleApp/Program.cs
@@ -25,4 +25,17 @@ var electronicCategory = new Category(1, "Electronic", DateTime.Now, "User1");
 
 Console.WriteLine($"Electronc Category {electronicCategory}");
 
+//Line Feed
+long month = new Random().NextInt64(11);
+string Season = $"The season is {month switch
+{
+    1 or 2 or 12 => "winter",
+    > 2 and < 6 => "spring",
+    > 5 and < 9 => "summer",
+    > 8 and < 12 => "autumn",
+    _ => "Unknown. Wrong month number",
+}}.";
+
+Console.WriteLine(Season);
+
 Console.ReadKey();
