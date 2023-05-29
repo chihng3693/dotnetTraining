a9298b36908bdcf06362976993ac9324a5704f41
eswaribala <parameswaribala@gmail.com>
2023-05-29 08:12:46 +0530

    updated

---
+++ EcommerceConsoleApp/EcommerceConsoleApp/Models/Category.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EcommerceConsoleApp.Models;
+
+//immutable
+public record Category
+(long CategoryId, string? CategoryName,DateTime CreatedDate, string? CreatedBy);
+   
+    ////initialized at constructor level
+    //public long CategoryId { get; init; }
+    //public string? CategoryName { get; init; }
+    ////nullability test
+    //public DateTime? CreatedDate { get; init; }
+    //public DateTime? ModifiedDate { get; set;}
+    //public string? CreatedBy { get; init; }
+    //public string? ModifiedBy { get; set; }
+    //public bool? IsActive { get; set; }
+
+//}
---
+++ EcommerceConsoleApp/EcommerceConsoleApp/Program.cs
@@ -0,0 +1,28 @@
+﻿// See https://aka.ms/new-console-template for more information
+//top level statements
+global using EcommerceConsoleApp.Models;
+
+Console.WriteLine("Rocking with .net core 7!!!");
+
+//instance for Category
+var electronicCategory = new Category(1, "Electronic", DateTime.Now, "User1");
+
+//immutability
+//electronicCategory.CategoryId = 47868;
+//init
+//electronicCategory.ModifiedBy = "User2";
+//set
+//electronicCategory.ModifiedBy = "User2";
+//electronicCategory.ModifiedDate = new DateTime(2023, 5, 5);
+
+
+//var furnitureCategory = electronicCategory with
+//{
+//    CategoryId = new Random().NextInt64(20000),
+//    CategoryName = "Furniture"
+//};
+//Console.WriteLine($"Furniture Category{ furnitureCategory}");
+
+Console.WriteLine($"Electronc Category {electronicCategory}");
+
+Console.ReadKey();
