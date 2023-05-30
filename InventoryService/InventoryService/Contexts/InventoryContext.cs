7947839274a5d4f7f3f76d3aef689bd4d8be56f5
eswaribala <parameswaribala@gmail.com>
2023-05-30 06:49:27 +0530

    updated

---
+++ InventoryService/InventoryService/Contexts/InventoryContext.cs
@@ -0,0 +1,17 @@
+﻿using InventoryService.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace InventoryService.Contexts
+{
+    public class InventoryContext:DbContext
+    {
+        public InventoryContext(DbContextOptions<InventoryContext> options) : base(options)
+        {
+            this.Database.EnsureCreated();
+        }
+
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Product> Products { get; set; }    
+
+    }
+}
--- InventoryService/InventoryService/Program.cs
+++ InventoryService/InventoryService/Program.cs
@@ -1,5 +1,7 @@
 using InventoryService.Configurations;
+using InventoryService.Contexts;
 using Microsoft.Data.SqlClient;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -14,15 +16,20 @@ SqlConnectionStringBuilder providerCs = new SqlConnectionStringBuilder();
 providerCs.InitialCatalog = data["dbname3"].ToString();
 providerCs.UserID = data["username"].ToString();
 providerCs.Password = data["password"].ToString();
-//providerCs.DataSource = "DESKTOP-55AGI0I\\MSSQLEXPRESS2021";
+//providerCs.DataSource = "DESKTOP-55AGI0I\\MSSQLEXPRESS2022";
+var machineName = data["machinename"];
+var serverName = data["servername"];
+var datasource = machineName + "\\" + serverName;
+providerCs.DataSource = datasource;
 //reading via config server
-providerCs.DataSource = configuration["servername"];
+//providerCs.DataSource = configuration["servername"];
 
 //providerCs.UserID = CryptoService2.Decrypt(ConfigurationManager.
 //AppSettings["UserId"]);
 providerCs.MultipleActiveResultSets = true;
 providerCs.TrustServerCertificate = false;
-
+builder.Services.AddDbContext<InventoryContext>(o =>
+o.UseSqlServer(providerCs.ToString()));
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
