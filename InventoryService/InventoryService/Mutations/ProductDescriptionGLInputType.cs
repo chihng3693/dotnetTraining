92e7bb7bbe1b6bc3e383f64381ee16ba00567a91
eswaribala <parameswaribala@gmail.com>
2023-05-31 06:54:34 +0530

    updated

---
+++ InventoryService/InventoryService/Mutations/ProductDescriptionGLInputType.cs
@@ -0,0 +1,17 @@
+﻿using GraphQL.Types;
+
+namespace InventoryService.Mutations
+{
+    public class ProductDescriptionGLInputType:InputObjectGraphType
+    {
+        public ProductDescriptionGLInputType()
+        {
+            Name = "ProductDescriptionInput";
+            Field<NonNullGraphType<StringGraphType>>("Summary");
+            Field<NonNullGraphType<LongGraphType>>("Cost");
+            Field<NonNullGraphType<DateGraphType>>("DOP");
+            Field<NonNullGraphType<DateGraphType>>("DOE");
+
+        }
+    }
+}
--- InventoryService/InventoryService/Mutations/RootMutation.cs
+++ InventoryService/InventoryService/Mutations/RootMutation.cs
@@ -9,10 +9,12 @@ namespace InventoryService.Mutations
     public class RootMutation:ObjectGraphType
     {
         private ICategoryRepo _categoryRepo;
+        private IProductRepo _productRepo;
 
-        public RootMutation(ICategoryRepo categoryRepo) { 
+        public RootMutation(ICategoryRepo categoryRepo,IProductRepo productRepo) { 
             
           _categoryRepo = categoryRepo;
+            _productRepo = productRepo;
 
             Name = "EcommerceMutation";
             Field<CategoryGLType>("insertCategory",
@@ -27,7 +29,52 @@ namespace InventoryService.Mutations
 
                 return InsertCategory(category);
             });
+            
+            Field<CategoryGLType>(
+               "UpdateCategory",
+               arguments: new QueryArguments(new QueryArgument<NonNullGraphType<LongGraphType>>
+               { Name = "CategoryId" },
+               new QueryArgument<NonNullGraphType<StringGraphType>>
+               { Name = "CategoryName" }
+               ),
+               resolve: context =>
+               {
+                   var categoryId = context.GetArgument<long>("CategoryId");
+                   var categoryName = context.GetArgument<string> ("CategoryName");
+                  
+                   return UpdateCategory(categoryId, categoryName);
+               }
+           );
 
+            Field<StringGraphType>(
+                "DeleteCategory",
+                arguments: new QueryArguments(new QueryArgument<NonNullGraphType<LongGraphType>>
+                { Name = "CategoryId" }),
+                resolve: context =>
+                {
+                    var categoryId = context.GetArgument<long>("CategoryId");
+
+                    categoryRepo.DeleteCategory(categoryId);
+                    return $"CategoryId {categoryId} is successfully deleted";
+                }
+            );
+
+
+            Field<ProductGLType>("insertProduct",
+            arguments: new QueryArguments(
+
+                new QueryArgument<ProductGLInputType> { Name = "product" },
+                new QueryArgument<NonNullGraphType<LongGraphType>> { Name = "catalogId" }),
+
+            resolve: context =>
+            {
+
+                var product = context.GetArgument<Product>("product");
+                var catalogId = context.GetArgument<long>("catalogId");
+
+                return InsertProduct(product,catalogId);
+            });
+            
 
         }
 
@@ -40,5 +87,23 @@ namespace InventoryService.Mutations
                 return await _categoryRepo.AddCategory(Category);
             }
         }
+        
+        private async Task<Category> UpdateCategory(long categoryId,string categoryName)
+        {
+
+            return await _categoryRepo.UpdateCategory(categoryId, categoryName);
+
+        }
+
+        private async Task<Product> InsertProduct(Product Product,long CatalogId)
+        {
+            if (Product == null)
+                return null;
+            else
+            {
+                return await _productRepo.AddProduct(Product, CatalogId);
+            }
+        }
+        
     }
 }
