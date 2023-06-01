c6593cb271086ede4e9ea95222f5bfe415be31c4
eswaribala <parameswaribala@gmail.com>
2023-06-01 13:55:50 +0530

    updated

--- GithubOAuth2App/GithubOAuth2App/Pages/Index.cshtml.cs
+++ GithubOAuth2App/GithubOAuth2App/Pages/Index.cshtml.cs
@@ -15,20 +15,21 @@ namespace GithubOAuth2App.Pages
         public IReadOnlyList<User> Followers { get; set; }
 
         public IReadOnlyList<User> Following { get; set; }
+        public string? AccessToken { get; set; }
 
         public async Task OnGetAsync()
         {
             if (User.Identity.IsAuthenticated)
             {
-                string accessToken = await HttpContext.GetTokenAsync("access_token");
+                AccessToken = await HttpContext.GetTokenAsync("access_token");
 
-                var github = new GitHubClient(new ProductHeaderValue("AspNetCoreGitHubAuth"), new InMemoryCredentialStore(new Credentials(accessToken)));
-                Repositories = await github.Repository.GetAllForCurrent();
+            //    var github = new GitHubClient(new ProductHeaderValue("AspNetCoreGitHubAuth"), new InMemoryCredentialStore(new Credentials(accessToken)));
+            //    Repositories = await github.Repository.GetAllForCurrent();
 
-                StarredRepos = await github.Activity.Starring.GetAllForCurrent();
+            //    StarredRepos = await github.Activity.Starring.GetAllForCurrent();
 
-                Followers = await github.User.Followers.GetAllForCurrent();
-                Following = await github.User.Followers.GetAllFollowingForCurrent();
+            //    Followers = await github.User.Followers.GetAllForCurrent();
+            //    Following = await github.User.Followers.GetAllFollowingForCurrent();
             }
         }
     }
