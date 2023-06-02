018b604653bc800050c4c6074ab50f1df3d7c41a
eswaribala <parameswaribala@gmail.com>
2023-06-02 14:09:03 +0530

    updated

--- GrpcClient/Program.cs
+++ GrpcClient/Program.cs
@@ -1,2 +1,13 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+//Console.WriteLine("Hello, World!");
+
+
+using Grpc.Net.Client;
+using GrpcServiceDemo;
+
+var data = new HelloRequest { Name = "Parameswari" };
+var grpcChannel = GrpcChannel.ForAddress("https://localhost:7286");
+//delete grpc service in .csproj build and then add
+var client = new Greeter.GreeterClient(grpcChannel);
+var response = await client.SayHelloAsync(data);
+Console.WriteLine(response.Message);
